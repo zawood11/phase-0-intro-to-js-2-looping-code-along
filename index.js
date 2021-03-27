8c8f17ddeec798f632ece70f379ec18f9bedb6c4
Zach <zawood11@gmail.com>
2021-03-27 14:26:13 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const occasion = 'surprise';
+
+function writeCards(names, occasion){
+
+    const thankYouArray = []
+    for (let i = 0; i < names.length; i++){
+        thankYouArray.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+        debugger;
+    }
+    
+    return thankYouArray;
+}
+
+function countDown(integer){
+
+    while (integer >= 0){
+        console.log(integer--)
+    }
+}
